7f034079733f5abb4f2518b329c498447103aba3
Romain G <rom1guyot@gmail.com>
2014-01-02 17:51:37 +0100

    Tuto 1 terminé
    voir Tutorial\tuto 1 - simple.html

--- Tutorial/implementation/tutorial_1/proj.py
+++ Tutorial/implementation/tutorial_1/proj.py
@@ -1,7 +1,7 @@
 from shaderComp.core import *
 from shaderComp.shaders import *
 
-myProj = Project.Project('tutorial 1')
+myProj = Project.Project('firstProject')
 
 myColor = Color.Color()
 myDefaultVertexShader = DefaultVertexShader.DefaultVertexShader()
@@ -16,7 +16,6 @@ vertexInVertVar = myProj.addVertexInVar('vertex', 'vec4')
 vertexOutFinalPositionVar = myProj.addVertexOutVar('final_position', 'vec4')
 pixelOutFinalColor = myProj.addPixelOutVar('final_color', 'vec4')
 
-
 myProj.addLink(vertexInVertVar, myDefaultVertexShader.getInVar('vertex'))
 myProj.addLink(myDefaultVertexShader.getOutVar('position'), vertexOutFinalPositionVar)
 myProj.addLink(myColor.getOutVar('color'), pixelOutFinalColor)
@@ -25,5 +24,5 @@ myProj.addLink(myProj.getVertexPipelineVar('Vertex'), vertexInVertVar)
 myProj.addLink(vertexOutFinalPositionVar, myProj.getVertexPipelineVar('Position'))
 myProj.addLink(pixelOutFinalColor, myProj.getPixelPipelineVar('FragColor'))
 
-myProj.compute('GLSLPrinter')
-myProj.render()
+myProj.compute('GLSLPrinter');
+myProj.render();
